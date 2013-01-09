fa78e11c574507d4286f15fa642e2c432adf772a
roderick3427 <roderickjackson3427@gmail.com>
2013-01-08 18:57:52 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wiyknotpre::Application.config.secret_token = 'c015730bb23ad1acfbc0568014701d94b6158e39d6fffd515051b7e1325ad2d732fb221dd05239e65266a4f4de2d24427f4924ae5bcb47c17209f8e226bcd58a'
